dd31fa904cc928dacf4c23121d06585e73fe1c8b
Alexandre Poly <alex.p1@hotmail.fr>
2020-12-15 09:59:40 +0100

    commit

---
+++ src/Entity/Film.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $annee_sortie;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $resume;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getAnneeSortie(): ?int
+    {
+        return $this->annee_sortie;
+    }
+
+    public function setAnneeSortie(int $annee_sortie): self
+    {
+        $this->annee_sortie = $annee_sortie;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+}
